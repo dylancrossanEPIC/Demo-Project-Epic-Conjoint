501194dead3b275e1c389ca4ea8f345ca4b3f820
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-01 18:39:10 +0000

    creating my first course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=200)
+    course_length = models.IntegerField(default=0)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Course, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
