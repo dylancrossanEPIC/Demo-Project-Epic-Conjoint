db266ab2bccc62e6e852335e2ca41349d4ffb3be
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-01 22:32:35 +0000

    Created REST-API that can CRUD now need to create front end to capture this data and send to our backend

--- courses/api.py
+++ courses/api.py
@@ -17,12 +17,12 @@ def course(request, course_id: int):
         return 404, {"message":"Course does not exist"}
 
 @api.post("/courses", response={201: CourseSchema})
-def createCourse(request, course: CourseSchema):
+def create_course(request, course: CourseSchema):
     course = Course.objects.create(**course.dict())
     return course
 
 @api.put("/courses/{course_id}", response={200: CourseSchema, 404: NotFoundSchema})
-def course(request, course_id: int, data: CourseSchema):
+def update_course(request, course_id: int, data: CourseSchema):
     try:
         course = Course.objects.get(pk=course_id)
         for attribute, value in data.dict().items():
@@ -31,3 +31,12 @@ def course(request, course_id: int, data: CourseSchema):
         return 200, course
     except Course.DoesNotExist as e:
         return 404, {"message":"Course does not exist"}
+
+@api.delete("/courses/{course_id}", response={200: None, 404: NotFoundSchema})
+def delete_course(request, course_id: int):
+    try:
+        course = Course.objects.get(pk=course_id)
+        course.delete()
+        return 200
+    except Course.DoesNotExist as e:
+        return 404, {"message": "Could not find course"}
