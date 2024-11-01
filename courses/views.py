ecfdefe5a304110f72e67f69e8c7f9451b8fab3d
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-01 23:27:33 +0000

    setting up front end enviroment with basic index.html page

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django import forms
+from  django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello sir")
+    return render(request, 'index.html')
