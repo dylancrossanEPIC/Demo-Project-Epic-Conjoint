f4f1ce05108f8badf1f046a0dff5b9a2f0254f61
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-02 19:53:56 +0000

    retrieved data from courses endpoint and passed that data to my index.html view and displayed in very simple html table layout

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
-
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create_course", views.create_course, name="create_course"),
-    path("update_course", views.update_course, name="update_course"),
+    path("create-course", views.create_course, name="create_course"),
+    path("update-course", views.update_course, name="update_course"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
-
+from django.http import HttpRequest
+import requests
+import json
 
 def index(request):
-    return render(request, 'index.html')
+    response = requests.get('http://127.0.0.1:8000/api/courses')
+    courses = response.json()
+    return render(request, 'index.html',{'courses':courses})
 
 def create_course(request):
     return render(request, 'createcourses.html')
