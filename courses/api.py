fdeec039e0abe47633d2de9621cbf3010c95efba
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-07 13:46:38 +0000

    Refractoring code moving all database operations into class methods that the api calls to instead of running them in the api file

--- courses/api.py
+++ courses/api.py
@@ -6,37 +6,20 @@ from courses.schema import CourseSchema, NotFoundSchema, CreateCourseSchema
 api = NinjaAPI()
 @api.get("/courses", response=List[CourseSchema])
 def courses(request):
-    return Course.objects.all()
+    return Course.getAllCourses()
 
 @api.get("/courses/{course_id}", response={200: CourseSchema, 404: NotFoundSchema})
 def course(request, course_id: int):
-    try:
-        course = Course.objects.get(pk=course_id)
-        return 200, course
-    except Course.DoesNotExist as e:
-        return 404, {"message":"Course does not exist"}
+    return Course.getCourseByID(course_id)
 
 @api.post("/courses", response={201: CreateCourseSchema})
 def create_course(request, course: CreateCourseSchema):
-    course = Course.objects.create(**course.dict())
-    return course
+    return Course.createCourse(course)
 
 @api.put("/courses/{course_id}", response={200: CourseSchema, 404: NotFoundSchema})
 def update_course(request, course_id: int, data: CourseSchema):
-    try:
-        course = Course.objects.get(pk=course_id)
-        for attribute, value in data.dict().items():
-            setattr(course, attribute, value)
-        course.save()
-        return 200, course
-    except Course.DoesNotExist as e:
-        return 404, {"message":"Course does not exist"}
+    return Course.updateCourse(course_id, data)
 
 @api.delete("/courses/{course_id}", response={200: None, 404: NotFoundSchema})
 def delete_course(request, course_id: int):
-    try:
-        course = Course.objects.get(pk=course_id)
-        course.delete()
-        return 200
-    except Course.DoesNotExist as e:
-        return 404, {"message": "Could not find course"}
+    return Course.deleteCourse(course_id)
--- courses/models.py
+++ courses/models.py
@@ -7,4 +7,36 @@ class Course(models.Model):
     course_details = models.CharField(max_length=200,default='')
     course_pub_date = models.DateField()
     def __str__(self):
-        return self.course_title   
+        return self.course_title
+    
+
+    def getAllCourses():
+        return Course.objects.all()
+    
+    def getCourseByID(course_id):
+        try:
+            course = Course.objects.get(pk=course_id)
+            return 200, course
+        except Course.DoesNotExist as e:
+            return 404, {"message":"Course does not exist"}
+
+    def createCourse(course):
+        return Course.objects.create(**course.dict())
+
+    def updateCourse(course_id, data):
+        try:
+            course = Course.objects.get(pk=course_id)
+            for attribute, value in data.dict().items():
+                setattr(course, attribute, value)
+            course.save()
+            return 200, course
+        except Course.DoesNotExist as e:
+            return 404, {"message":"Course does not exist"}
+        
+    def deleteCourse(course_id):
+        try:
+            course = Course.objects.get(pk=course_id)
+            course.delete()
+            return 200
+        except Course.DoesNotExist as e:
+            return 404, {"message": "Could not find course"}
