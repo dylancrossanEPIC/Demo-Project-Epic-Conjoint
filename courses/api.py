93f2311fd4f3330b092e91fa377a660591c56508
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-10 15:16:15 +0000

    test

--- courses/api.py
+++ courses/api.py
@@ -1,6 +1,10 @@
 from ninja import NinjaAPI
 from courses.routers.coursesRouter import course_router
 
+
 api = NinjaAPI()
 
 api.add_router("/courses", course_router)
+
+from ninja_extra import NinjaExtraAPI
+
