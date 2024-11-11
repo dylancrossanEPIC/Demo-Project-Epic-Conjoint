daca0f5fece3eed35028b32a10af06e63340e463
Dylan Crossan EPIC <dylancrossanepic@Dylans-MacBook-Air.local>
2024-11-11 14:42:42 +0000

    Added a Data mappers folder to isolate my database operations from the Course class to stop them from being interdependent on each other

--- courses/routers/coursesRouter.py
+++ courses/routers/coursesRouter.py
@@ -1,6 +1,6 @@
 import json
 from ninja import Router
-from courses.models.courseModel import Course
+from courses.mappers.courseMapper import CourseMapper
 from courses.schemas.courseSchema import CourseSchema, NotFoundSchema, CreateCourseSchema
 from typing import List
 
@@ -11,24 +11,24 @@ class CourseAPI:
 
     @course_router.get("", response=List[CourseSchema])
     def courses(request):
-        courses = Course.getAllCourses()
+        courses = CourseMapper.getAllCourses()
         return [CourseSchema(**course) for course in courses]
 
 
     @course_router.get("/{course_id}", response={200: CourseSchema, 404: NotFoundSchema})
     def course(request, course_id: int):
-        course = Course.getCourseByID(course_id)
+        course = CourseMapper.getCourseByID(course_id)
         return course
 
 
     @course_router.post("", response={201: CreateCourseSchema})
     def create_course(request, course: CreateCourseSchema):
-        return Course.createCourse(course)
+        return CourseMapper.createCourse(course)
 
     @course_router.put("/{course_id}", response={200: CourseSchema, 404: NotFoundSchema})
     def update_course(request, course_id: int, data: CourseSchema):
-        return Course.updateCourse(course_id, data)
+        return CourseMapper.updateCourse(course_id, data)
 
     @course_router.delete("/{course_id}", response={200: None, 404: NotFoundSchema})
     def delete_course(request, course_id: int):
-        return Course.deleteCourse(course_id)
+        return CourseMapper.deleteCourse(course_id)
